8a8a1da35773102f8c004a618617307cec17a329
Finn <finn@finn.io>
2017-04-11 08:31:17 -0700

    minor improvements to the html

--- mapmaker.py
+++ mapmaker.py
@@ -8,6 +8,9 @@ markers = []
 center = [0, 0]
 for address in table:
     markers.append(dict(address))
+    if len(markers) == 0:
+        center[0] = address.get('lat')
+        center[1] = address.get('lon')
     center[0] = (address.get('lat') + center[0])/2
     center[1] = (address.get('lon') + center[1])/2
 print("var mapdata = %s" % json.dumps({"center": center, "markers": markers}))
